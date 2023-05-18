8bb66bb406625af3c9a412ee3e965acf569b6bfd
MohsinRehman12 <mshakeelrehman@hotmail.com>
2023-05-17 22:29:11 -0400

    added API reducers and implemented it in app

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -1,12 +1,34 @@
 import { useState, useEffect } from 'react';
 import { copy, linkIcon, loader, tick } from '../assets';
+import  { useLazyGetSummaryQuery } from '../services/article';
 
 const Demo = () => {
   const [article, setArticle] = useState({
     url:'',summary:''
   });
+
+  const [getSummary, {error, isFetching}] = useLazyGetSummaryQuery();
+  const [allArticles, setAllArticles] = useState([]);
+  
+  useEffect(() => {
+    const savedArticles = JSON.parse(localStorage.getItem('articles'));
+    if (savedArticles) {
+      setAllArticles(savedArticles);
+    }
+  }, []);
+  
   const handleSubmit = async (e) => {
-    alert('Submitted');
+    e.preventDefault();
+    const { data } = await getSummary({articleUrl: article.url});
+
+    if (data?.summary) {
+      const newArticle = { ...article, summary: data.summary };
+      const updatedAllArticles = [...allArticles, newArticle];
+      setAllArticles(updatedAllArticles);
+      setArticle(newArticle);
+      localStorage.setItem('articles', JSON.stringify(updatedAllArticles));
+    }
+
   }
   return (
     <section className="mt-16 w-full max-w-xl">
@@ -38,7 +60,63 @@ const Demo = () => {
           >
           </button>
         </form>
+
+        <div className="flex flex-col gap-1 max-h-60 overflow-y-auto">
+
+              {allArticles.map((article, index) => (
+                <div
+                key={`link-${index}`}
+                onClick={() => setArticle(article)}
+                className='link_card'
+                >
+
+                  <div className="copy_btn">
+                    <img 
+                    src={copy}
+                    alt='copy_icon'
+                    className='w-[40%] h-[40%] object-contain'
+                    />
+                  </div>
+                  <p className='flex-1 font-satoshi text-blue-700 text-sm truncate'>
+                    {article.url}
+                  </p>
+
+                </div>
+              ))}
+
+        </div>
         
+      </div>
+
+      <div className="my-10 max-w-full flex justify-center items-center">
+        {isFetching ? (
+          <img src={loader} alt="loader" className="w-20 h-20 object-contain" />
+        ) : error ? (
+
+          <p className="font-inter font-bold text-black text-center">
+            Error has occured, We Apologize for the issue
+            <br />
+            
+            <span className="font-satoshi font-normal text-gray-700">
+              {error?.data?.error}
+            </span>
+
+            </p>
+          ) : (
+            article.summary && (
+              <div className="flex flex-col gap-3">
+                <h2 className='font-satoshi font-bold text-gray-600 text-xl'>
+                  Article <span className="blue_gradient">Summary</span>
+                </h2>
+                <div className="summary_box">
+                  <p className="font-inter font-normal text-sm text-gray-700"
+                  >{article.summary}</p>
+                </div>
+              </div>
+
+              
+            )
+          )}
       </div>
     </section>
   )
